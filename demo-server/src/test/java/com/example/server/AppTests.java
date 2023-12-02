8c4cd42f12db18c891a0d7555af2740d7f6b974b
Freeman Liu <llw599502537@gmail.com>
2023-12-02 17:19:12 +0800

    upgrade 3.2.0-RC2

--- demo-server/src/test/java/com/example/server/AppTests.java
+++ demo-server/src/test/java/com/example/server/AppTests.java
@@ -1,13 +1,24 @@
 package com.example.server;
 
+import com.example.api.UserApi;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AppTests {
+
+    @Autowired
+    UserApi userApi;
 
     @Test
     void contextLoads() {
+        assertThat(userApi.getById("1"))
+                .isEqualTo(new UserApi.UserDTO("1", "Freeman", List.of("Coding", "Reading")));
     }
 
 }
