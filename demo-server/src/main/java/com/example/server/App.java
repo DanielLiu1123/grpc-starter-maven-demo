3b10329fc675255ed5e68aae6d70154df913e108
Freeman Liu <llw599502537@gmail.com>
2024-03-09 14:11:54 +0800

    Use protovalidate

--- demo-server/src/main/java/com/example/server/App.java
+++ demo-server/src/main/java/com/example/server/App.java
@@ -1,36 +1,41 @@
 package com.example.server;
 
 import com.freemanan.starter.grpc.client.EnableGrpcClients;
+import io.grpc.stub.StreamObserver;
 import io.grpc.testing.protobuf.SimpleRequest;
 import io.grpc.testing.protobuf.SimpleResponse;
 import io.grpc.testing.protobuf.SimpleServiceGrpc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableGrpcClients("io.grpc")
-public class App implements ApplicationRunner {
+public class App extends SimpleServiceGrpc.SimpleServiceImplBase {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    @Autowired
-    SimpleServiceGrpc.SimpleServiceBlockingStub simpleStub;
-
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        SimpleResponse resp = simpleStub.unaryRpc(
-                SimpleRequest.newBuilder()
-                        .setRequestMessage("Hello")
-                        .build()
-        );
-        log.info("Response: {}", resp.getResponseMessage());
+    public void unaryRpc(SimpleRequest request, StreamObserver<SimpleResponse> responseObserver) {
+        SimpleResponse response = SimpleResponse.newBuilder()
+                .setResponseMessage("Your request: " + request.getRequestMessage())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Bean
+    ApplicationRunner runner(SimpleServiceGrpc.SimpleServiceBlockingStub simpleStub) {
+        return args -> {
+            SimpleResponse resp = simpleStub.unaryRpc(
+                    SimpleRequest.newBuilder().setRequestMessage("Hello").build());
+            log.info("Response: {}", resp.getResponseMessage());
+        };
     }
 }
